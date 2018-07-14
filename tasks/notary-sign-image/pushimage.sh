6071ca487595b40dea18461537a4875c23c2983d
prasenforu <prasenforu@hotmail.com>
2018-07-14 08:44:40 +0530

    Create pushimage.sh

---
+++ tasks/notary-sign-image/pushimage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+source /opt/resource/common.sh
+
+
+start_docker
+
+
+docker login $HARBOR_URL -u $HARBOR_USERNAME -p $HARBOR_PASSWORD
+docker pull $HARBOR_IMAGE
+docker images
+docker push $HARBOR_IMAGE 2>/dev/null
